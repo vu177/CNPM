6b60046165df25ee3a507d23511a009ccf12b786
vu177 <49463031+vu177@users.noreply.github.com>
2020-06-16 23:18:32 +0700

    commit

---
+++ CNPM_CHUNGKHOAN/CNPM_CHUNGKHOAN/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace CNPM_CHUNGKHOAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbngay1 = new System.Windows.Forms.Label();
+            this.lbNgay = new System.Windows.Forms.Label();
+            this.bANGGIATRUCTUYENDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bANGGIATRUCTUYENBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cHUNGKHOANDataSet = new CNPM_CHUNGKHOAN.CHUNGKHOANDataSet();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableAdapterManager = new CNPM_CHUNGKHOAN.CHUNGKHOANDataSetTableAdapters.TableAdapterManager();
+            this.bANGGIATRUCTUYENTableAdapter = new CNPM_CHUNGKHOAN.CHUNGKHOANDataSetTableAdapters.BANGGIATRUCTUYENTableAdapter();
+            this.lbChange = new System.Windows.Forms.ListBox();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bANGGIATRUCTUYENDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bANGGIATRUCTUYENBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cHUNGKHOANDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(364, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(492, 25);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "BẢNG GIÁ TRỰC TUYẾN CÁC GIAO DỊCH TRONG NGÀY";
+            // 
+            // lbngay1
+            // 
+            this.lbngay1.AutoSize = true;
+            this.lbngay1.Location = new System.Drawing.Point(945, 88);
+            this.lbngay1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbngay1.Name = "lbngay1";
+            this.lbngay1.Size = new System.Drawing.Size(0, 17);
+            this.lbngay1.TabIndex = 13;
+            // 
+            // lbNgay
+            // 
+            this.lbNgay.AutoSize = true;
+            this.lbNgay.Location = new System.Drawing.Point(672, 268);
+            this.lbNgay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbNgay.Name = "lbNgay";
+            this.lbNgay.Size = new System.Drawing.Size(0, 17);
+            this.lbNgay.TabIndex = 11;
+            // 
+            // bANGGIATRUCTUYENDataGridView
+            // 
+            this.bANGGIATRUCTUYENDataGridView.AutoGenerateColumns = false;
+            this.bANGGIATRUCTUYENDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.bANGGIATRUCTUYENDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11});
+            this.bANGGIATRUCTUYENDataGridView.DataSource = this.bANGGIATRUCTUYENBindingSource;
+            this.bANGGIATRUCTUYENDataGridView.Location = new System.Drawing.Point(87, 129);
+            this.bANGGIATRUCTUYENDataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.bANGGIATRUCTUYENDataGridView.Name = "bANGGIATRUCTUYENDataGridView";
+            this.bANGGIATRUCTUYENDataGridView.ReadOnly = true;
+            this.bANGGIATRUCTUYENDataGridView.Size = new System.Drawing.Size(1144, 304);
+            this.bANGGIATRUCTUYENDataGridView.TabIndex = 9;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "MACP";
+            this.dataGridViewTextBoxColumn1.HeaderText = "MACP";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "GIAMUA2";
+            this.dataGridViewTextBoxColumn2.HeaderText = "GIAMUA2";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "KLMUA2";
+            this.dataGridViewTextBoxColumn3.HeaderText = "KLMUA2";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "GIAMUA1";
+            this.dataGridViewTextBoxColumn4.HeaderText = "GIAMUA1";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "KLMUA1";
+            this.dataGridViewTextBoxColumn5.HeaderText = "KLMUA1";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "GIAKHOP";
+            this.dataGridViewTextBoxColumn6.HeaderText = "GIAKHOP";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "KLKHOP";
+            this.dataGridViewTextBoxColumn7.HeaderText = "KLKHOP";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.DataPropertyName = "GIABAN1";
+            this.dataGridViewTextBoxColumn8.HeaderText = "GIABAN1";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.DataPropertyName = "KLBAN1";
+            this.dataGridViewTextBoxColumn9.HeaderText = "KLBAN1";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.DataPropertyName = "GIABAN2";
+            this.dataGridViewTextBoxColumn10.HeaderText = "GIABAN2";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.DataPropertyName = "KLBAN2";
+            this.dataGridViewTextBoxColumn11.HeaderText = "KLBAN2";
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            this.dataGridViewTextBoxColumn11.ReadOnly = true;
+            // 
+            // bANGGIATRUCTUYENBindingSource
+            // 
+            this.bANGGIATRUCTUYENBindingSource.DataMember = "BANGGIATRUCTUYEN";
+            this.bANGGIATRUCTUYENBindingSource.DataSource = this.cHUNGKHOANDataSet;
+            // 
+            // cHUNGKHOANDataSet
+            // 
+            this.cHUNGKHOANDataSet.DataSetName = "CHUNGKHOANDataSet";
+            this.cHUNGKHOANDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(683, 278);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(415, 459);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 46);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "GIAO DỊCH";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.BANGGIATRUCTUYENTableAdapter = this.bANGGIATRUCTUYENTableAdapter;
+            this.tableAdapterManager.LENHDATTableAdapter = null;
+            this.tableAdapterManager.LENHKHOPTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = CNPM_CHUNGKHOAN.CHUNGKHOANDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // bANGGIATRUCTUYENTableAdapter
+            // 
+            this.bANGGIATRUCTUYENTableAdapter.ClearBeforeFill = true;
+            // 
+            // lbChange
+            // 
+            this.lbChange.FormattingEnabled = true;
+            this.lbChange.ItemHeight = 16;
+            this.lbChange.Location = new System.Drawing.Point(116, 54);
+            this.lbChange.Margin = new System.Windows.Forms.Padding(4);
+            this.lbChange.Name = "lbChange";
+            this.lbChange.Size = new System.Drawing.Size(184, 52);
+            this.lbChange.TabIndex = 14;
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thoat.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_thoat.Location = new System.Drawing.Point(744, 459);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(171, 46);
+            this.btn_thoat.TabIndex = 25;
+            this.btn_thoat.Text = "THOÁT";
+            this.btn_thoat.UseVisualStyleBackColor = false;
+            this.btn_thoat.Click += new System.EventHandler(this.btn_thoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 559);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbngay1);
+            this.Controls.Add(this.lbNgay);
+            this.Controls.Add(this.bANGGIATRUCTUYENDataGridView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbChange);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bANGGIATRUCTUYENDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bANGGIATRUCTUYENBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cHUNGKHOANDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbngay1;
+        private System.Windows.Forms.Label lbNgay;
+        private System.Windows.Forms.DataGridView bANGGIATRUCTUYENDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.BindingSource bANGGIATRUCTUYENBindingSource;
+        private CHUNGKHOANDataSet cHUNGKHOANDataSet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private CHUNGKHOANDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private CHUNGKHOANDataSetTableAdapters.BANGGIATRUCTUYENTableAdapter bANGGIATRUCTUYENTableAdapter;
+        private System.Windows.Forms.ListBox lbChange;
+        private System.Windows.Forms.Button btn_thoat;
+    }
+}
+
---
+++ CNPM_CHUNGKHOAN/CNPM_CHUNGKHOAN/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CNPM_CHUNGKHOAN
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+  
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+        }
+
+        internal static SqlDataReader ExecSqlDataReader(string strlenh)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
